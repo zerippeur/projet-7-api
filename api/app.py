210bc6ba4aacc39bfece772c0bce5353fa45f101
zerippeur <zerippeur@protonmail.com>
2024-01-31 21:58:47 +0100

    Working shap feature importance, global and local

--- api/app.py
+++ api/app.py
@@ -4,16 +4,19 @@ from fastapi import FastAPI
 import pickle
 import pandas as pd
 from xgboost import XGBClassifier
-from pydantic import BaseModel
 import mlflow.sklearn
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+import shap
 
 # 2. Create app and model objects
 app = FastAPI()
 
 model_path ='C:/Users/emile/DEV/WORKSPACE/projet-7-cours-oc/model/model/mlartifacts/675174994878903245/f160483f83af458da90360f354519e24/artifacts/XGBClassifier_model__bayes_search__randomundersampler_balancing__debug_run'
 model = mlflow.sklearn.load_model(model_path)
+
+api_data_for_shap_initiation = None
+explainer = None
 
 @app.post('/predict_from_dict')
 def predict_credit_risk(client_infos: dict):
@@ -34,7 +37,7 @@ def predict_credit_risk(client_infos: dict):
     }
 
 @app.post('/global_feature_importance')
-def get_global_feature_importance():
+async def get_global_feature_importance():
     if isinstance(model, XGBClassifier):
         feature_importance_dict = {
             'model_type': 'XGBClassifier',
@@ -50,6 +53,45 @@ def get_global_feature_importance():
             'feature_importance': model.feature_importances_
         }
     return feature_importance_dict
+
+@app.post('/initiate_shap_explainer')
+async def initiate_shap_explainer(data_for_shap_initiation: dict):
+    global api_data_for_shap_initiation
+    global explainer
+    if api_data_for_shap_initiation is None:
+        api_data_for_shap_initiation = pd.DataFrame.from_dict(data_for_shap_initiation, orient='index')
+        explainer = shap.Explainer(model)
+
+@app.post('/shap_feature_importance')
+async def get_shap_feature_importance(shap_feature_importance_dict: dict):
+    feature_scale = shap_feature_importance_dict['feature_scale']
+    if feature_scale == 'Global':
+        data2 = api_data_for_shap_initiation.to_dict(orient='index')
+        shap_values = explainer.shap_values(api_data_for_shap_initiation).tolist()
+        expected_value = None
+    elif feature_scale == 'Local':
+        client_infos = pd.DataFrame.from_dict(shap_feature_importance_dict['client_infos'], orient='index').T
+        data2 = client_infos.to_dict(orient='index')
+        shap_values = explainer.shap_values(client_infos).tolist()
+        expected_value = explainer.expected_value.item()
+
+    feature_names = model.feature_names_in_.tolist()
+
+    shap_values_dict = {
+        'data': data2,
+        'shap_values': shap_values,
+        'feature_names': feature_names,
+        'expected_value': expected_value
+    }
+
+    return shap_values_dict
+
+@app.post('/feature_names_in')
+def get_feature_names_in():
+    feature_names = {
+        'columns': model.feature_names_in_
+    }
+    return feature_names
 
 # 4. Run the API with uvicorn (uvicorn app:app --reload)
 # first app stands for the pyhton file, second app for the API instance, --reload for automatic refresh
